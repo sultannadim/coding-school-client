4ab5290b7d09c0282553e2a066c8cdc564662a7b
sultan5218 <sultan5218@gmail.com>
2022-10-25 23:57:44 +0600

    footer add

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,18 @@
+import React from "react";
+import "./Footer.css";
+
+const Footer = () => {
+  return (
+    <footer className="bg-dark py-3">
+      <div className="container">
+        <div className="footer">
+          <p className="m-0 text-center text-light">
+            All right reserved by Coding School.
+          </p>
+        </div>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
