c41f8881cde8f48b32369c7b1282379efc9fca1c
sultan5218 <sultan5218@gmail.com>
2022-10-25 23:46:37 +0600

    router setup complete

---
+++ src/Pages/Regestration/Regestration.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Regestration = () => {
+  return (
+    <div>
+      <h1>this is registration page</h1>
+    </div>
+  );
+};
+
+export default Regestration;
--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,9 +1,41 @@
 import { createBrowserRouter } from "react-router-dom";
 import Main from "../layout/Main";
+import Blog from "../Pages/Blog/Blog";
+import Courses from "../Pages/Courses/Courses";
+import Faq from "../Pages/Faq/Faq";
+import Home from "../Pages/Home/Home";
+import Login from "../Pages/Login/Login";
+import Regestration from "../Pages/Regestration/Regestration";
 
 export const router = createBrowserRouter([
   {
     path: "/",
     element: <Main></Main>,
+    children: [
+      {
+        path: "/",
+        element: <Home></Home>,
+      },
+      {
+        path: "courses",
+        element: <Courses></Courses>,
+      },
+      {
+        path: "faq",
+        element: <Faq></Faq>,
+      },
+      {
+        path: "blog",
+        element: <Blog></Blog>,
+      },
+      {
+        path: "login",
+        element: <Login></Login>,
+      },
+      {
+        path: "registration",
+        element: <Regestration></Regestration>,
+      },
+    ],
   },
 ]);
