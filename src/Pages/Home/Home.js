d3fddd8196f3cbeeae41e5e02551c34fb74a0a50
sultan5218 <sultan5218@gmail.com>
2022-10-26 00:13:35 +0600

    banner part complete

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -1,10 +1,21 @@
 import React from "react";
+import "./Home.css";
 
 const Home = () => {
   return (
-    <div>
-      <h1>This is home</h1>
-    </div>
+    <section className="banner">
+      <div className="container">
+        <div className="row justify-content-center">
+          <div className="col-lg-6">
+            <div className="banner-content">
+              <h1 className="fw-fw-bold m-0 text-center text-light">
+                Learn your way with Coding School to Programing
+              </h1>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
   );
 };
 
